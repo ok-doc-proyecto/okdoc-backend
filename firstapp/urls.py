04611371a3798950ae7f21152bdc2319223e69a7
tomasmonicat <tomasmonicat@gmail.com>
2023-07-18 15:21:01 +0200

    created four html templates and the url routing
    login, home, docprofile and userprofile html basic templates created.
    Basic url routing in django done.º

---
+++ firstapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'), 
+    path('login/', views.login, name='login'), 
+    path('docprofile/', views.docprofile, name='docprofile'), 
+    path('userprofile/', views.userprofile, name='userprofile'), 
+]
