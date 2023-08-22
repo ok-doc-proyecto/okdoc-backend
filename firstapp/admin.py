becaa0c1571d2f109dfeadb38b161b087e479aec
tomasmonicat <tomasmonicat@gmail.com>
2023-08-22 16:13:53 +0200

    Empieza la database con Especialidad y Medico
    Especialidad es simplemente una lista de especialidades médicas,
    Medico usa nombre, apellido, especialidad, fecha de agregado y
    rating.
    
    - El rating por ahora es inventado, no toma de reviews reales.º

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Especialidad, Medico
+
 # Register your models here.
+admin.site.register(Especialidad)
+admin.site.register(Medico)
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Especialidad(models.Model):
+    especialidad = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['especialidad']
+        verbose_name_plural = 'especialidades'
+
+    def __str__(self):
+        return self.especialidad
+    
+
+class Medico(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return ' '.join([self.name, self.surname])
