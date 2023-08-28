e21915578de67406ceb4bb110f389c8fe47abb67
tomasmonicat <tomasmonicat@gmail.com>
2023-08-28 16:01:58 +0200

    Agregado de CORS headers
    Se agregó CORS headers para permitir la conexión de APIs desde
    otros puertos. Actualmente el backend está en localhost:8000 y el
    frontend en localhost:5173. Esto se debe configurar en settings.py
    del django project si cambia.

--- firstapp/serializers.py
+++ firstapp/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 
 from .models import Especialidad, Medico, Review
 
+
+class EspecialidadSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Especialidad
+        fields = '__all__'
+
+
 class MedicoSerializer(serializers.ModelSerializer):
+    especialidad = EspecialidadSerializer()
 
     class Meta:
         model = Medico
--- okdoc/settings.py
+++ okdoc/settings.py
@@ -39,13 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'firstapp'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
