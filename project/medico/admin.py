91a72fb625ab1fa49e37335059da09227cc027b7
tomasmonicat <tomasmonicat@gmail.com>
2023-09-25 14:53:18 +0200

    Se agregó una tabla custom de usuarios
    Los usuarios tienen:
    - email
    - nombre
    - apellido
    - fecha de nacimiento (opcional)
    - fecha en que se agregaron

--- project/medico/admin.py
+++ project/medico/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Especialidad, Medico, RelMedicoEspecialidad, Review
+from .models import Especialidad, ObraSocial, Medico, Review, RelMedicoEspecialidad, RelMedicoObraSocial, Usuario
 
 # Register your models here.
 admin.site.register(Especialidad)
+admin.site.register(ObraSocial)
 admin.site.register(Medico)
-admin.site.register(RelMedicoEspecialidad)
 admin.site.register(Review)
+
+admin.site.register(RelMedicoEspecialidad)
+admin.site.register(RelMedicoObraSocial)
+
+admin.site.register(Usuario)
--- project/medico/models.py
+++ project/medico/models.py
@@ -1,7 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
 from django.db.models import Avg
 
 # Create your models here.
+class AccountsManager(BaseUserManager):
+
+    def create_superuser(self, email, first_name, last_name, password, **otros):
+        
+        otros.setdefault('is_staff', True)
+        otros.setdefault('is_superuser', True)
+
+        return self.create_user(email, first_name, last_name, password, **otros)
+
+    def create_user(self, email, first_name, last_name, password, **otros):
+        if not email:
+            raise ValueError('You must provide an email.')
+        if not first_name:
+            raise ValueError('You must provide an first_name.')
+        if not last_name:
+            raise ValueError('You must provide an last_name.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **otros)
+        user.set_password(password)
+        user.save()
+        return user
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_birth = models.DateField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = AccountsManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+
 class Especialidad(models.Model):
     especialidad = models.CharField(max_length=255)
 
